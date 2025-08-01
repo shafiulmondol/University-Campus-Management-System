2300d583cb42eca5d9d32886d8ab555009401b7f
Toimoon Islam Estia <toimoonislam@gmail.com>
2025-08-01 22:31:34 +0600

    update account_officer

---
+++ accountofficer.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 01, 2025 at 06:20 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `skst_university`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accountofficer`
+--
+
+CREATE TABLE `accountofficer` (
+  `officer_id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `full_name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `avatar_initials` char(2) NOT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `accountofficer`
+--
+
+INSERT INTO `accountofficer` (`officer_id`, `username`, `password`, `full_name`, `email`, `avatar_initials`, `last_login`, `created_at`) VALUES
+(233031, 'tanjila@gmail.com', '23303137', 'Tanjila Islam', 'tanjila@gmail.com', 'JD', '2025-07-29 00:00:00', '2025-07-29 16:12:55');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `accountofficer`
+--
+ALTER TABLE `accountofficer`
+  ADD PRIMARY KEY (`officer_id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `accountofficer`
+--
+ALTER TABLE `accountofficer`
+  MODIFY `officer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=233032;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
